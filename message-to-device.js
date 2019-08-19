8bd7be893cd89bf27839352aa7f8b67784ea2444
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2019-08-19 17:17:57 +0530

    Create message-to-device.js

---
+++ message-to-device.js
@@ -0,0 +1,19 @@
+// message-to-device.js
+
+//sending a message from backend code such as web service/mobile app etc
+
+// Using the Azure CLI:
+// az iot hub show-connection-string --hub-name {YourIoTHubName} --output table
+var connectionString = '<<>>';
+ 
+var Client = require('azure-iothub').Client;
+
+var deviceId = '<<<>>>';
+
+// Connect to the service-side endpoint on your IoT hub.
+var client = Client.fromConnectionString(connectionString);
+ 
+
+client.send(deviceId, "Message 2 coming from App", function callback() {
+    console.log('message sent ');
+})
